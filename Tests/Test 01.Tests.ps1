67e64b82f6616dce3f8d2202745726896c26694b
Rob <Rob@TheBeard.local>
2017-04-26 21:44:20 +0100

    Added atest for database

--- Tests/Test 01.Tests.ps1
+++ Tests/Test 01.Tests.ps1
@@ -1,6 +1,12 @@
 Describe "Here is a Test Block" {
+    BeforeAll {
+    $srv = New-Object Microsoft.SqlServer.Management.Smo.Server .\SQL2016
+    }
     It "Should have a good message now " {
         $true | Should be $true
+    }
+    It "Should have a database" {
+        $srv.Databases.Name -contains 'ProviderDemo' | Should Be $true
     }
 }
 
